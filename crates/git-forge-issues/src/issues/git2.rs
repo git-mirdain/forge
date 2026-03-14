798700f156e95715d1c9be4ea2deefd6f4fea3f9
Joseph D. Carpinelli <joseph.d.carpinelli@loopy.codes>
2026-03-14 12:09:10 -0400

    fixup! feat: add IssueOpts for customizable ref prefix

--- crates/git-forge-issues/src/issues/git2.rs
+++ crates/git-forge-issues/src/issues/git2.rs
@@ -2,18 +2,26 @@
 
 use git2::Repository;
 
-use crate::issues::{Issue, IssueState, Issues};
+use crate::issues::{Issue, IssueOpts, IssueState, Issues};
 
 impl Issues for Repository {
-    fn list_issues(&self) -> Result<Vec<Issue>, git2::Error> {
+    fn list_issues(&self, _opts: Option<&IssueOpts>) -> Result<Vec<Issue>, git2::Error> {
         todo!()
     }
 
-    fn list_issues_by_state(&self, _state: IssueState) -> Result<Vec<Issue>, git2::Error> {
+    fn list_issues_by_state(
+        &self,
+        _state: IssueState,
+        _opts: Option<&IssueOpts>,
+    ) -> Result<Vec<Issue>, git2::Error> {
         todo!()
     }
 
-    fn find_issue(&self, _id: u64) -> Result<Option<Issue>, git2::Error> {
+    fn find_issue(
+        &self,
+        _id: u64,
+        _opts: Option<&IssueOpts>,
+    ) -> Result<Option<Issue>, git2::Error> {
         todo!()
     }
 
@@ -23,6 +31,7 @@ impl Issues for Repository {
         _body: &str,
         _labels: &[String],
         _assignees: &[String],
+        _opts: Option<&IssueOpts>,
     ) -> Result<u64, git2::Error> {
         todo!()
     }
@@ -35,11 +44,18 @@ impl Issues for Repository {
         _labels: Option<&[String]>,
         _assignees: Option<&[String]>,
         _state: Option<IssueState>,
+        _opts: Option<&IssueOpts>,
     ) -> Result<(), git2::Error> {
         todo!()
     }
 
-    fn add_issue_comment(&self, _id: u64, _author: &str, _body: &str) -> Result<(), git2::Error> {
+    fn add_issue_comment(
+        &self,
+        _id: u64,
+        _author: &str,
+        _body: &str,
+        _opts: Option<&IssueOpts>,
+    ) -> Result<(), git2::Error> {
         todo!()
     }
 }
